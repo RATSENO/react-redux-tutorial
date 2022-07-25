11d53f3e475c3c078aa70e7e6e99717d0addad25
RATSENO <onestar1014@gmail.com>
2022-07-25 11:16:03 +0900

    connect - 2

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -12,20 +12,17 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-//리덕스 스토어 안의 상태를 컴포넌트 props로 넘겨주기 위해 설정하는 함수
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => {
+      dispatch(increase());
+    },
 
-//액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
-const mapDispatchToProps = (dispatch) => ({
-  increase: () => {
-    dispatch(increase());
-  },
-
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+    decrease: () => {
+      dispatch(decrease());
+    },
+  }),
+)(CounterContainer);
