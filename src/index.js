d5ca9a49d8308b1483ca6a57f452056afb0ee2f7
RATSENO <onestar1014@gmail.com>
2022-07-25 10:50:22 +0900

    redux 브라우저 익스텐션 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit'
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = configureStore({reducer: rootReducer, devTools: composeWithDevTools()});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
